c7919377e88ec09460436d72452b28ac1c44c17c
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2018-12-15 16:12:20 +0100

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,54 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("My window");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setVgap(8);
+        gridPane.setHgap(10);
+
+        Label nameLabel = new Label("Username");
+        GridPane.setConstraints(nameLabel, 0, 0);
+
+        TextField nameInput = new TextField();
+        nameInput.setPromptText("username");
+        GridPane.setConstraints(nameInput, 1, 0);
+
+        Label passLabel = new Label("Password");
+        GridPane.setConstraints(passLabel, 0, 1);
+
+        TextField passInput = new TextField();
+        passInput.setPromptText("password");
+        GridPane.setConstraints(passInput, 1, 1);
+
+        Button loginButton = new Button("Log In");
+        GridPane.setConstraints(loginButton, 1, 2);
+
+        gridPane.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
+
+        Scene scene = new Scene(gridPane, 500, 400);
+        window.setScene(scene);
+        window.show();
+    }
+
+}
